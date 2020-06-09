1cb6ada03dad96d5f6e9d303160e941c98d73bb8
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-09 18:27:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(keys, values) final_array|
+    
+    value.each do |inner_key, names|
+      binding.pry
+    end
+  end
+  binding.pry
 end
+
+
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+nyc_pigeon_organizer(pigeon_data)
