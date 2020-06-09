33963ad3c41da916f12d20afc4c7e326cda0ddd0
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-09 18:36:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name| 
         if !final_array[name]
-          final_array
+          final_array[name] = {}
     end
   end
   binding.pry
