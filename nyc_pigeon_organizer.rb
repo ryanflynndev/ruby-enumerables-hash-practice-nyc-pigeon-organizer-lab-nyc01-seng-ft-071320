691fbb82aa1bc34108d982ed80a62cd833fe9c02
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-09 18:34:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name| 
-        
+        if !final_array[name]
+          final_array
     end
   end
   binding.pry
